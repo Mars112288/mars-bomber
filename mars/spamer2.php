9b7f81094f3bfef92676b6fd83c08a019bf3f865
Mars112288 <hibayhosein@gmail.com>
2025-12-25 19:39:24 -0800

    Add files via upload

---
+++ mars/spamer2.php
@@ -0,0 +1,18 @@
+<?php 
+//#######[get]#######\\
+$shomare1 = $_GET [ "shomare" ] ; 
+$i = 1;
+while  ( $i <= 3) 
+{
+    //ILIYA MARS\\
+	$like = file_get_contents("جمشید کس مادرت از طرف لوتوسی و مارس و کیارش مکستر");
+	$i++;
+}
+	echo 'درخواست شما با موفقیت انجام شد <br> <a href="@nnyes">سازنده</a>'
+
+
+
+
+// ILIYAMARS \\ 
+?>
+
